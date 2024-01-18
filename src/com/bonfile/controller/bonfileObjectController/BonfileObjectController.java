80395ef10f092d276da160eea6e5dc3bb90916a6
Igor Sousa dos Santos Santana <igorssant@hotmail.com>
2024-01-18 13:32:54 -0300

    BonfileObjectController implementation started

--- src/com/bonfile/controller/bonfileObjectController/BonfileObjectController.java
+++ src/com/bonfile/controller/bonfileObjectController/BonfileObjectController.java
@@ -1,5 +1,23 @@
 package com.bonfile.controller.bonfileObjectController;
 
+import com.bonfile.model.bonfileObject.BonfileObject;
+
 public class BonfileObjectController {
-    /**/
+    private BonfileObject bonfileObject;
+
+    public BonfileObjectController() {
+        this.bonfileObject = new BonfileObject();
+    }
+
+    public BonfileObjectController(String objectName) {
+        this.bonfileObject = new BonfileObject(objectName);
+    }
+
+    public BonfileObjectController(String objectName, String className) {
+        this.bonfileObject = new BonfileObject(objectName, className);
+    }
+
+    public BonfileObjectController(BonfileObject bonfileObject) {
+        this.bonfileObject = bonfileObject;
+    }
 }
